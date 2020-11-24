ac94c1043c40578469bfa24e3e989a666c447921
raultomasmora <50747627+raultomasmora@users.noreply.github.com>
2020-11-24 07:15:33 +0100

    Update ML_energy_app.py

--- ML_energy_app.py
+++ ML_energy_app.py
@@ -4,7 +4,7 @@ import streamlit as st
 import pandas as pd
 import pickle
 import numpy as np
-from pycaret.regression import *
+from pycaret.regression import load_model, predict_model
 
 
 # entorno streamlit
