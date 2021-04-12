eba00a260d77bb39f4734f5c02d461da0650ece4
raultomasmora <50747627+raultomasmora@users.noreply.github.com>
2021-04-12 06:41:30 +0200

    Update ML_energy_app.py

--- ML_energy_app.py
+++ ML_energy_app.py
@@ -27,7 +27,7 @@ except:
     print("Se necesita un modelo entrenado")
 
 st.title('Certificación energética con Machine Learning'
-         '\n\n')
+         '\n\n ')
 st.error('Entorno web en pruebas... (actualización 2021-04-11)')
 with st.beta_expander("Información:", expanded=True):
     st.success('El proyecto ha sido elaborado por el investigador [Raúl Mora-García](https://publons.com/researcher/1717710/raul-tomas-mora-garcia/) [:email:](mailto:rtmg@ua.es) en colaboración con [Grupo Valero](https://www.grupovalero.com/) durante el año 2020. Subvención AEST/2019/005 del Programa para la promoción de la investigación científica, el desarrollo tecnológico y la innovación en la Comunitat Valenciana (Anexo VII) [DOGV nº8355](http://www.dogv.gva.es/datos/2018/08/06/pdf/2018_7758.pdf).')
@@ -40,17 +40,13 @@ st.sidebar.image('logo.png')
 st.sidebar.header('Parámetros de entrada')
 
 def user_input_features():
-    NORMATIVA_CONSTRUCCIO = st.sidebar.selectbox('Normativa de construcción', 
-                            ("Ant_NBECT79", "NBECT79", "CTE2006"))
+    NORMATIVA_CONSTRUCCIO = st.sidebar.selectbox('Normativa de construcción', ("Ant_NBECT79", "NBECT79", "CTE2006"), 
+                                                help=('Se definen tres periodos normativos según el año de construcción'))
     SUPERFICI_HAB = st.sidebar.slider('Superficie habitable (m²)', 20. , 250. , 70. , 
-                                      help="""This is a
-                                            very long string,
-                                            containing commas,
-                                            that I split up
-                                            for readability""".replace('\n',' '))
+                                      help=('Superficie útil de la vivienda'))
     COMPACITAT = st.sidebar.slider('Compacidad (m³/m²)', 0.5 , 18. , 3.4 , 
-                                   help=('Hola mundo'
-                                        '\n\n esto es una prueba, no se lo largo que puede entrar el texto en esta caja'))
+                                   help=('Compacidad calculada conforme al CTE'
+                                        '\n\n (volumen/área de la envolvente)'))
     VENTILACIO_USO_RESIDENCIAL = st.sidebar.slider('Ventilación uso residencial (renovaciones/hora)', 0.3 , 1. , 0.65 )
     VENTILACIO_INFILTRACIONS = st.sidebar.slider('Ventilación por infiltraciones (renovaciones/hora)', 0.4 , 1.3 , 0.63 )
     DEMANDA_ACS = st.sidebar.slider('Demanda de ACS (litros/día)', 0. , 1122. , 71.5 )
